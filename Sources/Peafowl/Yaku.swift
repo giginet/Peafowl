912a53c2318b18096f8eae14755f973c79ace415
giginet <giginet.net@gmail.com>
2018-06-27 03:19:28 +0900

    Implement Yaku

--- Sources/Peafowl/Yaku.swift
+++ Sources/Peafowl/Yaku.swift
@@ -1,9 +1,12 @@
 import Foundation
 
+/// 役
 public protocol Yaku {
+    associatedtype Form
+    
     var openedHan: Int { get }
     var closedHan: Int? { get }
-    func validate(with hand: Hand) -> Bool
+    func validate(with tiles: Form, drawed: Tile) -> Bool
 }
 
 public extension Yaku {
@@ -11,3 +14,12 @@ public extension Yaku {
         return 0
     }
 }
+
+/// 和了形
+public protocol OrdinaryFormedYaku: Yaku where Form == (Pair, Set, Set, Set, Set) { }
+
+/// 七対子形
+public protocol SevenPairFormedYaku: Yaku where Form == (Pair, Pair, Pair, Pair, Pair, Pair, Pair) { }
+
+/// 国士無双形
+public protocol ThirteenOrphansFormedYaku: Yaku where Form == Hand { }
