d16aa606b51bc6d6c6031322089d455250881a8f
giginet <giginet.net@gmail.com>
2018-06-27 02:49:32 +0900

    Add methods to Tiles

--- Sources/Peafowl/Tile.swift
+++ Sources/Peafowl/Tile.swift
@@ -40,4 +40,49 @@ public enum Tile: Equatable, Comparable {
     case fortune
     /// 中
     case center
+    
+    /// 数牌
+    public var isSuit: Bool {
+        switch self {
+        case .character, .dots, .bamboo:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    /// 字牌
+    public var isHonor: Bool {
+        return !isSuit
+    }
+    
+    /// 風牌
+    public var isWind: Bool {
+        switch self {
+        case .east, .south, .west, .north:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    /// 三元牌
+    public var isDragon: Bool {
+        switch self {
+        case .blank, .fortune, .center:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    /// 端牌
+    public var isTerminal: Bool {
+        switch self {
+        case .dots(let n), .character(let n), .bamboo(let n):
+            return n == 1 || n == 9
+        default:
+            return false
+        }
+    }
 }
