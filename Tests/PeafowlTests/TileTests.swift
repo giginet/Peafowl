64921ce80e1f8d121ebc4166d3c3b5b1c0292491
giginet <giginet.net@gmail.com>
2018-07-25 00:04:00 +0900

    Add tests for Tile

---
+++ Tests/PeafowlTests/TileTests.swift
@@ -0,0 +1,41 @@
+import Foundation
+import XCTest
+import Peafowl
+
+final class TileTests: XCTestCase {
+    let tiles: [Tile] = [
+        1.筒!,
+        5.索!,
+        9.萬!,
+        .center,
+        .blank,
+        .north,
+        .south,
+    ]
+    
+    private func assertTiles(_ results: [Bool], checker: (Tile) -> Bool) {
+        for (tile, result) in zip(tiles, results) {
+            XCTAssertEqual(checker(tile), result)
+        }
+    }
+    
+    func testSuit() {
+        assertTiles([true, true, true, false, false, false, false]) { $0.isSuit }
+    }
+    
+    func testHonor() {
+        assertTiles([false, false, false, true, true, true, true]) { $0.isHonor }
+    }
+    
+    func testWind() {
+        assertTiles([false, false, false, false, false, true, true]) { $0.isWind }
+    }
+    
+    func testDragon() {
+        assertTiles([false, false, false, true, true, false, false]) { $0.isDragon }
+    }
+    
+    func testTerminal() {
+        assertTiles([true, false, true, false, false, false, false]) { $0.isTerminal }
+    }
+}
