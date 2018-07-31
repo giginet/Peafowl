ea76731a70acf49c33138c5ad783ce3f2529480f
giginet <giginet.net@gmail.com>
2018-08-01 02:00:17 +0900

    Add aliases for Tiles

---
+++ Sources/Peafowl/Tile+Aliases.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+public let 🀇 = 1.萬
+public let 🀈 = 2.萬
+public let 🀉 = 3.萬
+public let 🀊 = 4.萬
+public let 🀋 = 5.萬
+public let 🀌 = 6.萬
+public let 🀍 = 7.萬
+public let 🀎 = 8.萬
+public let 🀏 = 9.萬
+public let 🀐 = 1.索
+public let 🀑 = 2.索
+public let 🀒 = 3.索
+public let 🀓 = 4.索
+public let 🀔 = 5.索
+public let 🀕 = 6.索
+public let 🀖 = 7.索
+public let 🀗 = 8.索
+public let 🀘 = 9.索
+public let 🀙 = 1.筒
+public let 🀚 = 2.筒
+public let 🀛 = 3.筒
+public let 🀜 = 4.筒
+public let 🀝 = 5.筒
+public let 🀞 = 6.筒
+public let 🀟 = 7.筒
+public let 🀠 = 8.筒
+public let 🀡 = 9.筒
+public let 🀀 = Tile.east
+public let 🀁 = Tile.south
+public let 🀂 = Tile.west
+public let 🀃 = Tile.north
+public let 🀄 = Tile.center
+public let 🀅 = Tile.fortune
+public let 🀆 = Tile.blank
