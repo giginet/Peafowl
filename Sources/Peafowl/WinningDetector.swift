f1f67a0a61d6e83423b4991d1bcc988f95884f68
giginet <giginet.net@gmail.com>
2018-08-12 01:31:06 +0900

    Implement WinningDetector

---
+++ Sources/Peafowl/WinningDetector.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+internal struct WinningDetector {
+    internal enum WinningForm {
+        case ordinary([TokenizedResult])
+        case sevenPairs
+        case thirteenOrphans
+    }
+    
+    func detectForms(_ tiles: [Tile]) -> [WinningForm]? {
+        if isThirteenOrphansForm(tiles) {
+            return [.thirteenOrphans]
+        }
+        
+        var results: [WinningForm] = []
+        if isSevenPairsWinningForm(tiles) {
+            results.append(.sevenPairs)
+        }
+        
+        let tokenizer = Tokenizer()
+        let tokenizedResults = tokenizer.tokenize(from: tiles)
+        if !tokenizedResults.isEmpty {
+            results.append(.ordinary(tokenizedResults))
+        }
+        if results.isEmpty {
+            return nil
+        }
+        return results
+    }
+    
+    private func isSevenPairsWinningForm(_ tiles: [Tile]) -> Bool {
+        let eyes = Tokenizer.findEyes(from: tiles)
+        return eyes.count == 7
+    }
+    
+    private func isThirteenOrphansForm(_ tiles: [Tile]) -> Bool {
+        let eyes = Tokenizer.findEyes(from: tiles)
+        return eyes.count == 1 && Set(tiles).count == 13
+    }
+}
