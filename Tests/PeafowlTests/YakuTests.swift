d3aa3410f66dabfb9eae08647636c76a7e8e1ee0
giginet <giginet.net@gmail.com>
2018-08-13 00:18:21 +0900

    Add tests for 七対子

--- Tests/PeafowlTests/YakuTests.swift
+++ Tests/PeafowlTests/YakuTests.swift
@@ -2,7 +2,7 @@ import Foundation
 import XCTest
 @testable import Peafowl
 
-final class YakuTests: XCTestCase {
+final class OrdinaryFormedYakuTests: XCTestCase {
     private let tokenizer = Tokenizer()
     
     private func searchWinningYaku<Yaku: YakuProtocol>(_ yaku: Yaku.Type, hand: Hand) -> [Yaku] {
@@ -29,5 +29,16 @@ final class YakuTests: XCTestCase {
     func test断ヤオ() {
         assert([二筒, 二筒, 三索, 四索, 五索, 三筒, 四筒, 五筒, 三萬, 四萬, 五萬, 六索, 七索, 八索], shouldBe: 断ヤオ九.self)
         assert([一筒, 一筒, 三索, 四索, 五索, 三筒, 四筒, 五筒, 三萬, 四萬, 五萬, 六索, 七索, 八索], shouldNotBe: 断ヤオ九.self)
+    }
+}
+
+final class SevenPairsFormedYakuTestx: XCTest {
+    func test七対子() {
+        let context = makeContext()
+        let hand0: Hand = [東, 東, 南, 南, 西, 西, 北, 北, 白, 白, 撥, 撥, 中, 中]
+        XCTAssertNotNil(七対子.make(with: hand0.allTiles, form: nil, picked: hand0.picked, context: context))
+        
+        let hand1: Hand = [東, 東, 東, 東, 西, 西, 北, 北, 白, 白, 撥, 撥, 中, 中]
+        XCTAssertNotNil(七対子.make(with: hand1.allTiles, form: nil, picked: hand1.picked, context: context))
     }
 }
